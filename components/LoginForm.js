f18c4992dda62c128a0398aa7b2789e74d89ac7f
iwpeifer <iwpeifer@gmail.com>
2017-06-06 19:56:03 -0400

    i hate poetry

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,25 +1,54 @@
 import React from 'react';
 
 export default class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+       username: "",
+       password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handlePwdChange = this.handlePwdChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(event){
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePwdChange(event){
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    if (this.state.username.length > 0 && this.state.password.length > 0) {
+      this.props.onSubmit()
+    } else {
+      return
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" value={this.state.username} onChange={this.handleChange} type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" value={this.state.password} onChange={this.handlePwdChange} type="password" />
           </label>
         </div>
         <div>
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -4,14 +4,62 @@ export default class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(event){
+    let validation = this.validate(this.state.value)
+    this.setState({
+      value: event.target.value
+    })
+    return validation
+  }
+
+  validate(value){
+    String.prototype.lines = function() { return this.split(/\r*\n/); }
+    String.prototype.lineCount = function() { return this.lines().length; }
+    console.log(value.lineCount())
+    // if (value.lineCount >= 1){
+    //   lineOne = value.lines()[0].split(" ")
+    // } else if (value.lineCount >= 2){
+    //   lineOne = value.lines()[0].split(" ")
+    //   lineTwo = value.lines()[1].split(" ")
+      if (value.lineCount() >= 3){
+        console.log("YES")
+      let lineOne = value.lines()[0].split(" ")
+      let lineTwo = value.lines()[1].split(" ")
+      let lineThree = value.lines()[2].split(" ")
+      if (value.lineCount() === 3 && lineOne.length === 5 && lineTwo.length === 3 && lineThree.length === 5){
+        return true
+        // document.getElementById("poem-validation-error").innerHTML = ""
+      } else {
+        return false
+        // document.getElementById("poem-validation-error").innerHTML = "This poem is not written in the right structure!"
+      }
+    }
+  }
+
+    // } else if (value. lineCount >= 2){
+    //     lineTwo = value.lines()[1].split(" ")
+    // } else if (value.lineCount >= 3){
+    //     lineThree = value.lines()[2].split(" ")
+    // }
+    // if (value.lineCount() === 3 && this.lineOne.length === 5 && this.lineTwo.length === 3 && this.lineThree.length === 5){
+    //   document.getElementById("poem-validation-error").innerHTML = ""
+    // } else {
+    //   document.getElementById("poem-validation-error").innerHTML = "This poem is not written in the right structure!"
+    // }
+  // }
+
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.value} onChange={this.handleChange} />
+        {this.handleChange ? null : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
